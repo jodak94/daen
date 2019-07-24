ebf4a2513fa453eab2bf2a5dade1c1db76c588b8
Jose <josedacak@gmail.com>
2019-07-24 12:21:18 -0400

    notifications

--- Themes/Adminlte/views/partials/notifications.blade.php
+++ Themes/Adminlte/views/partials/notifications.blade.php
@@ -12,6 +12,19 @@
     </div>
 @endif
 
+@if (Session::has('errors'))
+    <div class="alert alert-danger fade in alert-dismissable">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+        <ul>
+          @foreach ((array)json_decode(Session::get('errors')) as $campo)
+            @foreach ($campo as $error)
+              <li>{{$error}}</li>
+            @endforeach
+          @endforeach
+        </ul>
+    </div>
+@endif
+
 @if (Session::has('warning'))
     <div class="alert alert-warning fade in alert-dismissable">
         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
