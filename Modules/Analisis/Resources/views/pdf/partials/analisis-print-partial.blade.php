aef47ef5cd52ffff32a78fdea05a7135797af3b6
jodak94 <30419614+jodak94@users.noreply.github.com>
2019-08-31 11:57:50 -0400

    Update analisis-print-partial.blade.php

--- Modules/Analisis/Resources/views/pdf/partials/analisis-print-partial.blade.php
+++ Modules/Analisis/Resources/views/pdf/partials/analisis-print-partial.blade.php
@@ -20,18 +20,24 @@
         $ajuste_y = 0;
       @endphp
     @endif
+    @php
+      $y += $y_acu + $ajuste_y;
+    @endphp
     <div class="{{$action}} @if($seccion_actual != -1) margin-top @endif" style="position: absolute;left: {{ $boxes->titulo_resultado->x }}cm;top: {{ $y }}cm"><b>{{$resultado->determinacion->subseccion->seccion->titulo}}</b></div>
     @php
-      $y += $y_acu + $ajuste_y + 0.4;
+      $y += $y_acu + $ajuste_y;
     @endphp
   @endif
   {{-- -------------!SECCIONES-------------- --}}
 
   {{-- -------------SUBSECCIONES------------- --}}
   @if($subseccion_actual != $resultado->determinacion->subseccion->id && $resultado->mostrar_subtitulo)
+    @php
+      $y += $y_acu;
+    @endphp  
     <div class="{{$action}}" style="position: absolute;left: {{ $boxes->titulo_resultado->x }}cm;top: {{ $y }}cm"><u>{{$resultado->determinacion->subseccion->titulo}}</u></div>
     @php
-      $y += $y_acu + 0.4
+      $y += $y_acu
     @endphp
   @endif
   {{-- -------------!SUBSECCIONES------------- --}}
