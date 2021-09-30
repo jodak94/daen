ffc2fd6c5a2fdf710ae1455e487eb8cf2213f13d
jodak94 <30419614+jodak94@users.noreply.github.com>
2021-09-29 20:53:26 -0400

    Update Resultado.php

--- Modules/Analisis/Entities/Resultado.php
+++ Modules/Analisis/Entities/Resultado.php
@@ -16,7 +16,7 @@ class Resultado extends Model
 
     public function getValorAttribute(){
       if(is_numeric($this->attributes['valor'])){
-        return number_format($this->attributes['valor'], 2);
+        return (float) $this->attributes['valor'];
       }
 
       return $this->attributes['valor'];
